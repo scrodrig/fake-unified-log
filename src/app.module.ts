688e57f3ad2ed708b5a34ca13e8cf985721ac24b
Schubert Rodriguez Quezada <schubert.quezada@babelgroup.com>
2024-04-09 17:04:25 +0100

    update app module ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { FakeUnifiedLogsController } from './fake-unified-logs/fake-unified-logs.controller';
+import { FakeUnifiedLogsService } from './fake-unified-logs/fake-unified-logs.service';
+import { FakeUnifiedLogsModule } from './fake-unified-logs/fake-unified-logs.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [FakeUnifiedLogsModule],
+  controllers: [AppController, FakeUnifiedLogsController],
+  providers: [AppService, FakeUnifiedLogsService],
 })
 export class AppModule {}

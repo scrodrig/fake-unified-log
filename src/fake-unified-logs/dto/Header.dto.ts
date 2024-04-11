07c0c5c5697972d5b0407e4acfd25efe57d8ecc8
Schubert Rodriguez Quezada <schubert.quezada@babelgroup.com>
2024-04-11 14:46:12 +0100

    Update HeaderDTO to include additional request headers

--- src/fake-unified-logs/dto/Header.dto.ts
+++ src/fake-unified-logs/dto/Header.dto.ts
@@ -1,14 +1,49 @@
 import { Expose } from 'class-transformer';
-import { IsDefined, IsString } from 'class-validator';
+import { IsDateString, IsDefined, IsString } from 'class-validator';
 
 export class HeaderDTO {
   @IsString()
   @IsDefined()
-  @Expose({ name: 'myheader1' }) // required as headers are case insensitive
-  myHeader1: string;
+  @Expose({ name: 'client_id' }) // required as headers are case insensitive
+  clientId: string;
 
-  // @IsString()
-  // @IsDefined()
-  // @Expose({ name: 'myheader2' })
-  // myHeader2: string;
+  @IsString()
+  @IsDefined()
+  @Expose({ name: 'client_secret' })
+  clientSecret: string;
+
+  @IsString()
+  @IsDefined()
+  @Expose({ name: 'x-correlation-id' })
+  xCorrelationId: string;
+
+  @IsString()
+  @IsDefined()
+  @Expose({ name: 'x-api-name' })
+  xApiName: string;
+
+  @IsDateString()
+  @IsDefined()
+  @Expose({ name: 'x-timestamp' })
+  xTimeStamp: string;
+
+  @IsString()
+  @IsDefined()
+  @Expose({ name: 'x-user' })
+  xUser: string;
+
+  @IsString()
+  @IsDefined()
+  @Expose({ name: 'x-platform' })
+  xPlatform: string;
+
+  @IsString()
+  @IsDefined()
+  @Expose({ name: 'x-step' })
+  xStep: string;
+
+  @IsString()
+  @IsDefined()
+  @Expose({ name: 'Content-Type' || 'content-type' })
+  contentType: string;
 }

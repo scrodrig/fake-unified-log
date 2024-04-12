b26cd57ad305df9e39a4f7396940be468e1e8c79
Schubert Rodriguez Quezada <schubert.quezada@babelgroup.com>
2024-04-12 14:29:37 +0100

    Update PostFakeDto to include validation for content and requestMethod

--- src/fake-unified-logs/dto/PostFake.dto.ts
+++ src/fake-unified-logs/dto/PostFake.dto.ts
@@ -1,24 +1,35 @@
-import { IsDefined, IsEnum } from 'class-validator';
+import {
+  IsDefined,
+  IsEnum,
+  IsNotEmpty,
+  IsObject,
+  IsString,
+  ValidateNested,
+} from 'class-validator';
 import { FakeLogDto } from '../interfaces';
+import { ContentDTO } from './Content.dto';
+import { Type } from 'class-transformer';
 
 export class PostFakeDto implements FakeLogDto {
-  @IsEnum(['SAP', 'TIBCO', 'CENTRIC', 'CPI'])
+  @IsEnum(['Processes', 'Services', 'Interfaces'])
   @IsDefined()
   applicationType: 'Processes' | 'Services' | 'Interfaces';
+
+  @IsString()
+  @IsDefined()
+  @IsNotEmpty()
   domain: string;
-  content: {
-    inputPayload: string;
-    payload: string;
-    context: {
-      elpased: number;
-      isError: '0' | '1';
-      eCode: string;
-      eDescription: string;
-      nCode: string;
-      nDescription: string;
-      businessKeyGroups: '' | [object];
-    };
-  };
+
+  @ValidateNested()
+  @IsNotEmpty()
+  @Type(() => ContentDTO)
+  content: ContentDTO;
+
+  @IsEnum(['GET', 'POST', 'PUT'])
+  @IsDefined()
   requestMethod: 'GET' | 'POST' | 'PUT';
+
+  @IsString()
+  @IsDefined()
   rawRequestUri: string;
 }

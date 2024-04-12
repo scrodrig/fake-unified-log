eda909764f52de245c4e919e8ac15a0e29e58832
Schubert Rodriguez Quezada <schubert.quezada@babelgroup.com>
2024-04-12 11:52:24 +0100

    Update FakeLogDto interface to include additional properties

--- src/fake-unified-logs/interfaces/fake-log-dto.interface.ts
+++ src/fake-unified-logs/interfaces/fake-log-dto.interface.ts
@@ -1,6 +1,19 @@
 export interface FakeLogDto {
-  hostname: string;
-  username: string;
-  email: string;
-  ip: string;
+  applicationType: 'Processes' | 'Services' | 'Interfaces';
+  domain: string;
+  content: {
+    inputPayload: string;
+    payload: string;
+    context: {
+      elpased: number;
+      isError: '0' | '1';
+      eCode: string;
+      eDescription: string;
+      nCode: string;
+      nDescription: string;
+      businessKeyGroups: [object] | '';
+    };
+  };
+  requestMethod: 'GET' | 'POST' | 'PUT';
+  rawRequestUri: string;
 }

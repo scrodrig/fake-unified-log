a645a86a1ab550ffba5f09b1616aa615edbb289c
Schubert Rodriguez Quezada <schubert.quezada@babelgroup.com>
2024-04-12 14:29:09 +0100

    Add ContentDTO class to handle content data in FakeUnifiedLogsController

---
+++ src/fake-unified-logs/dto/Content.dto.ts
@@ -0,0 +1,24 @@
+import {
+  IsDefined,
+  IsNotEmpty,
+  IsString,
+  ValidateNested,
+} from 'class-validator';
+import { ContextDTO } from './Context.dto';
+import { Type } from 'class-transformer';
+export class ContentDTO {
+  @IsString()
+  @IsDefined()
+  @IsNotEmpty()
+  inputPayload: string;
+
+  @IsString()
+  @IsDefined()
+  @IsNotEmpty()
+  payload: string;
+
+  @ValidateNested()
+  @IsNotEmpty()
+  @Type(() => ContextDTO)
+  context: ContextDTO;
+}

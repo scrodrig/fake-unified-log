3ca77d1eea5dd902f4ecc8dfb8c88cda2b112b91
Schubert Rodriguez Quezada <schubert.quezada@galp.com>
2024-04-18 10:54:43 +0100

    Add logger calls to start, end, flow, and exception methods in FakeUnifiedLogsService

--- src/fake-unified-logs/fake-unified-logs.service.ts
+++ src/fake-unified-logs/fake-unified-logs.service.ts
@@ -3,24 +3,29 @@ import { logs as fakeLogs } from './data/fake-logs';
 import { FakeLogDto, StatusResponse, FakeLog } from './interfaces';
 import { HeaderDTO } from './dto/Header.dto';
 import { mockRequest } from './utils/mock-request';
+import { logger } from './utils/Logger.utils';
 
 @Injectable()
 export class FakeUnifiedLogsService {
   private readonly logs: FakeLog[] = fakeLogs;
 
   start(headers: HeaderDTO, log: FakeLogDto): StatusResponse {
+    logger(headers, log, 'start');
     return mockRequest({ headers, log });
   }
 
   end(headers: HeaderDTO, log: FakeLogDto): StatusResponse {
+    logger(headers, log, 'end');
     return mockRequest({ headers, log });
   }
 
   flow(headers: HeaderDTO, log: FakeLogDto): StatusResponse {
+    logger(headers, log, 'flow');
     return mockRequest({ headers, log });
   }
 
   exception(headers: HeaderDTO, log: FakeLogDto): StatusResponse {
+    logger(headers, log, 'exception');
     return mockRequest({ headers, log });
   }
 
